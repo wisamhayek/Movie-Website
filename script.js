6380b363c26c215ca4684ba25685e3619a53aedf
Wisam Hayek <hayekwisam@gmail.com>
2021-11-22 14:36:36 -0800

    Create all files with header and images

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let movieID= 508;
 let chosenMovie =[];
 const fetchMovie = () => {
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${config.API_Key}`).then(response => {
+    // https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos,images
+    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${API_Key}`).then(response => {
                     if(response.status !== 200) { // I got some error
                         console.log("error");
                     }
@@ -13,12 +14,12 @@ const fetchMovie = () => {
                     chosenMovie = searchInput; //sign the search result to Global Variavel
 
                     //Append the result in the html
-                    for(let i=0; i<chosenMovie.length;i++){
-                        let displayInfo = document.createElement('span'); //create the element
-                        displayInfo.innerHTML = `<br><div class="card">ID: ${chosenMovie[i].id}<div>Name: ${chosenMovie[i].name}<div>Username: ${chosenMovie[i].username}</div><button class="submit btn btn-primary" onClick="deleteButton(this.value)" value="${i}" id="${i}deleteUser">Delete User</button>
-                                <button class="submit btn btn-primary"  value="${i}" id="${i}editUser">Edit User</button><br>`;
-                        document.getElementById("displayResult").appendChild(displayInfo);
-                    }
+                    // for(let i=0; i<chosenMovie.length;i++){
+                    //     let displayInfo = document.createElement('span'); //create the element
+                    //     displayInfo.innerHTML = `<br><div class="card">ID: ${chosenMovie[i].id}<div>Name: ${chosenMovie[i].name}<div>Username: ${chosenMovie[i].username}</div><button class="submit btn btn-primary" onClick="deleteButton(this.value)" value="${i}" id="${i}deleteUser">Delete User</button>
+                    //             <button class="submit btn btn-primary"  value="${i}" id="${i}editUser">Edit User</button><br>`;
+                    //     document.getElementById("displayResult").appendChild(displayInfo);
+                    // }
 
                     }).catch(error => { // catch the error
                         console.log(error)
