298183e8e971c2806a935f3ec9234f95a9be28ac
Wisam Hayek <hayekwisam@gmail.com>
2021-11-22 12:45:41 -0800

    Create all files with header

---
+++ config.js
@@ -0,0 +1,1 @@
+const API_Key="afb43adefc2304857d0e31f2cea7c34b";
---
+++ script.js
@@ -0,0 +1,27 @@
+let movieID= 508;
+let chosenMovie =[];
+const fetchMovie = () => {
+    fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${config.API_Key}`).then(response => {
+                    if(response.status !== 200) { // I got some error
+                        console.log("error");
+                    }
+                    if(response.status === 404){
+                        alert("Movie not found.")
+                     }
+                    //convert data
+                    response.json().then(searchInput => {
+                    chosenMovie = searchInput; //sign the search result to Global Variavel
+
+                    //Append the result in the html
+                    for(let i=0; i<chosenMovie.length;i++){
+                        let displayInfo = document.createElement('span'); //create the element
+                        displayInfo.innerHTML = `<br><div class="card">ID: ${chosenMovie[i].id}<div>Name: ${chosenMovie[i].name}<div>Username: ${chosenMovie[i].username}</div><button class="submit btn btn-primary" onClick="deleteButton(this.value)" value="${i}" id="${i}deleteUser">Delete User</button>
+                                <button class="submit btn btn-primary"  value="${i}" id="${i}editUser">Edit User</button><br>`;
+                        document.getElementById("displayResult").appendChild(displayInfo);
+                    }
+
+                    }).catch(error => { // catch the error
+                        console.log(error)
+                    })
+                })
+        }
